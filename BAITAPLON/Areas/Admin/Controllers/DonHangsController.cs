8976cb550e45ca9e14ac379780ae92a5d0e589fc
nguyenducanhanh <nguyenducanh260402@gmail.com>
2023-04-25 22:19:28 +0700

    Upload Admin Controller

---
+++ BAITAPLON/Areas/Admin/Controllers/DonHangsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BAITAPLON.Models;
+
+namespace BAITAPLON.Areas.Admin.Controllers
+{
+    public class DonHangsController : Controller
+    {
+        private QuanLySanPhamEntities db = new QuanLySanPhamEntities();
+
+        // GET: Admin/DonHangs
+        public ActionResult Index()
+        {
+            return View(db.DonHangs.ToList());
+        }
+
+        // GET: Admin/DonHangs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // GET: Admin/DonHangs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/DonHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,TenKH,SDT,GT,CachNhan")] DonHang donHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DonHangs.Add(donHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donHang);
+        }
+
+        // GET: Admin/DonHangs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // POST: Admin/DonHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,TenKH,SDT,GT,CachNhan")] DonHang donHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donHang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(donHang);
+        }
+
+        // GET: Admin/DonHangs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // POST: Admin/DonHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DonHang donHang = db.DonHangs.Find(id);
+            db.DonHangs.Remove(donHang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BAITAPLON/Areas/Admin/Controllers/NhanVienController.cs
@@ -0,0 +1,119 @@
+﻿using BAITAPLON.Models;
+using BAITAPLON.Models.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BAITAPLON.Areas.Admin.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        // GET: Admin/NhanVien
+
+        public bool KiemTraChucNang( int idChucNang)
+        {
+            QuanLySanPhamEntities db = new QuanLySanPhamEntities();
+            NhanVien nvSession = (NhanVien)Session["user"];
+            var count = db.PhanQuyens.Count(m => m.idNhanVien == nvSession.ID & m.idChucNang == idChucNang);
+            if (count == 0)
+            {
+                // báo ko có quyền 
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public ActionResult DanhSach()
+        {
+            if(KiemTraChucNang(1) == false)
+            {
+                return Redirect("/Admin/BaoLoi/KhongCoQuyen");
+            }
+
+            var map = new mapNhanVien();
+            return View(map.DanhSach());
+        }
+
+      
+
+        public ActionResult ThemMoi()
+        {
+            if (KiemTraChucNang(1) == false)
+            {
+                return Redirect("/Admin/BaoLoi/KhongCoQuyen");
+            }
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult ThemMoi(NhanVien model, HttpPostedFileBase file)
+        {
+
+
+
+
+            var map = new mapNhanVien();
+            if (map.ThemMoi(model) > 0)
+            {
+                return RedirectToAction("DanhSach");
+            }
+            else
+            {
+                ModelState.AddModelError("", map.message);
+                return View(model);
+            }
+
+        }
+
+        // update
+
+        public ActionResult CapNhat(int ID)
+        {
+            if (KiemTraChucNang(1) == false)
+            {
+                return Redirect("/Admin/BaoLoi/KhongCoQuyen");
+            }
+            var map = new mapNhanVien();
+            return View(map.ChiTiet(ID));
+        }
+
+      
+
+        [HttpPost]
+        public ActionResult CapNhat(NhanVien model, HttpPostedFileBase file)
+        {
+           
+            var map = new mapNhanVien();
+            if (map.CapNhat(model) > 0)
+            {
+                return RedirectToAction("DanhSach");
+            }
+            else
+            {
+                ModelState.AddModelError("", map.message);
+                return View(model);
+            }
+        }
+
+
+
+        // delete
+
+        public ActionResult Xoa(int id)
+        {
+            if (KiemTraChucNang(1) == false)
+            {
+                return Redirect("/Admin/BaoLoi/KhongCoQuyen");
+            }
+            var map = new mapNhanVien();
+            map.Xoa(id);
+            return RedirectToAction("DanhSach");
+        }
+      
+
+    }
+}
